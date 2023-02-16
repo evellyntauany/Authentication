3517a8b7ccd00b798410e6e9dd2e354cd20d6df7
evellyn <evirodrigues10@gmail.com>
2023-02-16 00:04:03 -0300

    Ajuste register

--- SistemaChamado/src/pages/Register/index.tsx
+++ SistemaChamado/src/pages/Register/index.tsx
@@ -1,10 +1,10 @@
-import { ChangeEvent, useContext, useState } from "react";
+import { ChangeEvent, FormEvent, useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/Auth/AuthContext";
 
 const Register = () => {
     
-    const auth = useContext(AuthContext)
+  const auth = useContext(AuthContext)
 
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
@@ -13,45 +13,41 @@ const Register = () => {
   const [error, setError] = useState('');
  
 
-const handleSubmit= async ()=>{
-  
+async function handleSubmit(e:FormEvent) {
+  e.preventDefault()
+  const user ={
+    name,
+    email,
+    password,
+  }
+  console.log(user)
 }
 
-const user ={
-  name,
-  email,
-  password
-}
 
-console.log(user)
-if (password !== confirmPassword){
-  setError("As senhas precisam ser iguais!")
-}
 
   return (
     <div>
       <h1>Login sistema</h1>
-      <form>
+      <form   onSubmit={handleSubmit}>
       <div>
           <label>Nome:</label>
-          <input name="name" type="text" required onChange={e=>setName(e.target.value)}/>
+          <input name="name" type="text" onChange={e=>setName(e.target.value)}/>
         </div>
         <div>
           <label>Email:</label>
-          <input name="email" type="email"  required onChange={e=>setEmail(e.target.value)}/>
+          <input name="email" type="email" onChange={e=>setEmail(e.target.value)}/>
         </div>
         <div>
           <label>Senha:</label>
-          <input name="password" type="password" required onChange={e=>setPassword(e.target.value)}></input>
+          <input name="password" type="number" onChange={e=>setPassword(e.target.value)}></input>
         </div>
         <div>
           <label>Confirmar senha:</label>
-          <input type="password" required onChange={e=>setConfirmPassword(e.target.value)}></input>
+          <input type="password" onChange={e=>setConfirmPassword(e.target.value)}></input>
         </div>
         <div>
           <button
           type="submit"
-          onClick={handleSubmit}
           >Entrar</button>
         </div>
         {error && <p>{error}</p>}
