3fa8d41eb26851e0ea6e8617e00899ac568cf509
evellyn <evirodrigues10@gmail.com>
2023-07-03 20:00:20 -0300

    att

--- backend/server/database/Service_order.js
+++ backend/server/database/Service_order.js
@@ -1,8 +1,7 @@
 const Sequelize = require('sequelize');
 const db = require('./dataBase');
 const User = require('./User');
-const Solicitacao = require('./Solicitacao_ServiceOrder');
-const Status = require('./Status_ServiceOrder');
+
 
 const Service_order = db.define('service_order', {
     idService_order: {
@@ -28,11 +27,6 @@ const Service_order = db.define('service_order', {
 // Define a relação entre Chamado e Usuário
 Service_order.belongsTo(User, { foreignKey: 'userId' });
 
-// Define a relação entre Chamado e Tipo de Solicitação
-Service_order.belongsTo(Solicitacao, { foreignKey: 'idSolicitacao_ServiceOrder' });
-
-// Define a relação entre Chamado e Tipo de Solicitação
-Service_order.belongsTo(Status, { foreignKey: 'idStatus_ServiceOrder' });
 
 // Verifica se a alteração na tabela
 Service_order.sync();
